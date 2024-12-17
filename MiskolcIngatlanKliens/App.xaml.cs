1b0ccabb276f4890f39ca1a275b1f34b992a7743
tatarjaras <havasim@kkszki.hu>
2024-12-17 12:20:29 +0100

    Add project files.

---
+++ MiskolcIngatlanKliens/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MiskolcIngatlanKliens
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
