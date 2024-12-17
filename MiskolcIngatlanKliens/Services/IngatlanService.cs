6296ede4a8f3dd3832dfa4e8f841ada36674964f
tatarjaras <havasim@kkszki.hu>
2024-12-17 13:24:39 +0100

    getall

--- MiskolcIngatlanKliens/Services/IngatlanService.cs
+++ MiskolcIngatlanKliens/Services/IngatlanService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MiskolcIngatlanKliens.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,10 @@ namespace MiskolcIngatlanKliens.Services
 {
     internal class IngatlanService
     {
+        public static async Task<List<Ingatlan>> GetAll(HttpClient client)
+        {
+
+            
+        }
     }
 }
