ed3ebd43b84b60ae40e2606f6d6efa3d1af445a3
tatarjaras <havasim@kkszki.hu>
2024-12-17 12:32:59 +0100

    models

---
+++ MiskolcIngatlanKliens/Models/Ingatlan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiskolcIngatlanKliens.Models;
+
+public partial class Ingatlan
+{
+    public int Id { get; set; }
+
+    public int Telepules { get; set; }
+
+    public int Cim { get; set; }
+
+    public int Ar { get; set; }
+
+    public int Tipus { get; set; }
+
+    public int KepNev { get; set; }
+
+    public int Ugyintezoid { get; set; }
+
+    public virtual Ugyintezo Ugyintezo { get; set; } = null!;
+}
---
+++ MiskolcIngatlanKliens/Models/Ugyintezo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MiskolcIngatlanKliens.Models;
+
+public partial class Ugyintezo
+{
+    public int Id { get; set; }
+
+    public string Nev { get; set; } = null!;
+
+    public string Telefon { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+    [JsonIgnore]
+
+    public virtual ICollection<Ingatlan> Ingatlans { get; set; } = new List<Ingatlan>();
+}
